784d64ace297744d6c7f1b58ab10d32b08c8b5bf
Frederik Scheerer <s8fksche@stud.uni-saarland.de>
2021-06-10 00:17:43 +0200

    nord colors

--- config.h
+++ config.h
@@ -7,11 +7,21 @@ static const char *fonts[] = {
 	"Mononoki Nerd Font:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static const char norm_fg[] = "#d8dee9";
+static const char norm_bg[] = "#2E3440";
+
+static const char norm_border[] = "#4C566A";
+static const char bar_border[] = "#4C566A";
+
+static const char sel_fg[] = "#eceff4";
+static const char sel_bg[] = "#4C566A";
+
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { norm_fg, norm_bg },
+	[SchemeSel] = { sel_fg, sel_bg },
+	[SchemeOut] = { norm_border, bar_border },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
